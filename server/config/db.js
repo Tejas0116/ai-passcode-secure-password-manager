787b1f3da7cb03583316aae0b2ee2d38412d8787
Tejas0116 <kesarkartejas27@gmail.com>
2026-06-19 22:31:18 +0530

    Fix MongoDB environment variable

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/password_manager_db');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
